6573b1a041c297e4930b9f7e19883e8757eb6b6b
Adam-fromSZTE <84191652+Adam-fromSZTE@users.noreply.github.com>
2022-04-07 00:14:03 +0200

    README added

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Test-DT
+React és Node.js.
+
+Indítás:
+
+Frontend mappán belül: yarn/npm start
+
+http://localhost:3000/cocktail
  
--- frontend/src/Service/api.js
+++ frontend/src/Service/api.js
@@ -8,11 +8,11 @@ class ApiService {
      return axios.get(SERVER_URL + endpoint);
    }
 
-   //Api which make a get to the server, in the params i give the name variable
+   //Api which make a get to the server, in the query i give the name variable
    searchDrink(name) {
       const endpoint = 'cocktails/';
       return axios.get(SERVER_URL + endpoint + name);
     }
  }
- 
+
  export default new ApiService();
